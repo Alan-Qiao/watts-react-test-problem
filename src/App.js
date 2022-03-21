4a319ac780226584a3e821bd1f8ad3a176c390f8
Alan-Qiao <alangxqiao@gmail.com>
2022-03-21 08:36:49 -0400

    Implemented and Tested Clickable Button

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      buttonThanks: false
     };
   }
 
   //handler goes here
+  handleClick() {
+    this.setState({ buttonThanks: !this.state.buttonThanks });
+  }
 
   render() {
     return (
       <div>
         <p>Greetings!</p>
-        <p>Button goes here</p>
+        <button data-testid="clicker" type="button" onClick={() => this.handleClick()}>{this.state.buttonThanks ? 'thanks' : 'click me'}</button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,25 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 
 // import your story from App.stories.js
-
+import { Primary } from './App.stories';
 
 // Add your tests here
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
+test('Button initially displays click me', () => {
+    render(<Primary />);
+    expect(screen.getByText(/click me/i)).toBeInTheDocument();
+});
+
+test('Button clicked changes to thanks', () => {
+    render(<Primary />);
+    fireEvent.click(screen.getByTestId('clicker'));
+    expect(screen.getByText(/thanks/i)).toBeInTheDocument();
+})
+
+test('Clicking button twice returns to click me', () => {
+    render(<Primary />);
+    fireEvent.click(screen.getByTestId('clicker'));
+    expect(screen.getByText(/thanks/i)).toBeInTheDocument();
+    fireEvent.click(screen.getByTestId('clicker'));
+    expect(screen.getByText(/click me/i)).toBeInTheDocument();
+})
